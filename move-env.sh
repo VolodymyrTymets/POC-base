1fa8773f4b39f025b0c17b013154ba7ed0d9921f
volodymyr <volodymyrtymets@gmail.com>
2026-09-18 10:58:23 +0300

    add move-env.sh to move env files to new directory structure

---
+++ move-env.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+echo "Moving api/.env to $1"
+cp $PWD/api/.env $1/api/
+cp $PWD/api/.env.development $1/api/
+cp $PWD/api/.env.test $1/api/
+
+
+echo "Moving web/.env to $1/web/..."
+cp $PWD/web/.env $1/web/
+cp $PWD/web/packages/app/.env $1/web/packages/app/
+cp $PWD/web/packages/admin/.env $1/web/packages/admin/
